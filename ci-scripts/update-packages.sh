2b3309c4acbfdaabf2d6e8cfd5ea067fafeaf868
thelamer <ryankuba@gmail.com>
2025-06-11 21:40:59 -0400

    ping syft tag

--- ci-scripts/update-packages.sh
+++ ci-scripts/update-packages.sh
@@ -8,7 +8,7 @@ docker build --build-arg REPO=${GITHUB_REPOSITORY,,} -t $IMAGE:latest .
 docker run --rm \
   -v /var/run/docker.sock:/var/run/docker.sock:ro \
   -v $(pwd):/tmp \
-  ghcr.io/anchore/syft:latest \
+  ghcr.io/anchore/syft:v1.26.1 \
   $IMAGE:latest -o table=/tmp/package_versions_check.txt
 if [ ! -f 'package_versions.txt' ]; then
   mv package_versions_check.txt package_versions.txt
